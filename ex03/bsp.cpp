916af041065fc2a4acd11ab344962d77618f99e3
aweaver <aweaver@student.42.fr>
2022-11-17 22:01:39 +0100

    WIP EX03:
    Working on ex03.
    Might validate the day without that.

---
+++ ex03/bsp.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsp.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 15:33:49 by aweaver           #+#    #+#             */
+/*   Updated: 2022/11/17 15:34:55 by aweaver          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsp.hpp"
+#include "Point.hpp"
+#include "Fixed.hpp"
+
+bool	bsp(Point const a, Point const b, Point const c, Point const point)
+{
+	return (0);
+	return (1);
+}
---
+++ ex03/bsp.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsp.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 15:32:21 by aweaver           #+#    #+#             */
+/*   Updated: 2022/11/17 15:33:46 by aweaver          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BSP_HPP
+# define BSP_HPP
+#include "Point.hpp"
+
+bool	bsp(Point const a, Point const b, Point const c, Point const point);
+
+#endif
